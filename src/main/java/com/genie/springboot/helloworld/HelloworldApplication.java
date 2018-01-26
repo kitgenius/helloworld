539b01eed799388209f1222ccab5debfd9e01e52
kitgenius <kitgenius@163.com>
2018-01-26 16:52:46 +0800

    httpclient

--- src/main/java/com/genie/springboot/helloworld/HelloworldApplication.java
+++ src/main/java/com/genie/springboot/helloworld/HelloworldApplication.java
@@ -1,5 +1,11 @@
 package com.genie.springboot.helloworld;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpPost;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +18,19 @@ public class HelloworldApplication {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		String pic = "F:\\temp\\123.png";
+		
+		File picFile = new File(pic);
+		HttpPost httpPost = new HttpPost("http://172.18.128.86:8080/faith_web/image/upload");// 创建httpPost 
+		
+		HttpEntity entity = 
+		httpPost.setEntity(entity);
+		
 		SpringApplication.run(HelloworldApplication.class, args);
+		
 	}
+	
+	
+	
 }
